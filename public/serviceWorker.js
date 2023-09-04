68a63092aca817c4f0ae33aab0060f919cfc2fad
Tim Koop <github@timkoop.com>
2023-09-04 16:26:19 -0500

    Try to cache star

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js')
 
 
-// workbox.routing.registerRoute(
-//   ({request}) => request.destination === 'image',
-//   new workbox.strategies.CacheFirst()
-// )
+workbox.routing.registerRoute(
+  new RegExp('/.*'),
+  new workbox.strategies.NetworkFirst()
+)
