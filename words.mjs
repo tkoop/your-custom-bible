96ae5b85183d0bf87e03ce3bf1d70e1f85dfda8a
Tim Koop <tim@timkoop.com>
2025-01-05 21:15:27 -0600

    Tweaked some words

--- words.mjs
+++ words.mjs
@@ -1,101 +1,137 @@
-var words = {}
-words['armor'] = {ca:'armour', gb:'armour', us:'armor'}
-words['armory'] = {ca:'armoury', gb:'armoury', us:'armory'}
-words['baptize'] = {ca:'baptize', gb:'baptise', us:'baptize'}
-words['behavior'] = {ca:'behaviour', gb:'behaviour', us:'behavior'}
-words['canceled'] = {ca:'cancelled', gb:'cancelled', us:'canceled'}
-words['center'] = {ca:'centre', gb:'centre', us:'center'}
-words['clamor'] = {ca:'clamour', gb:'clamour', us:'clamor'}
-words['counseled'] = {ca:'counselled', gb:'counselled', us:'counseled'}
-words['counselor'] = {ca:'counsellor', gb:'counsellor', us:'counselor'}
-words['defense'] = {ca:'defence', gb:'defence', us:'defense'}
-words['demeanor'] = {ca:'demeanour', gb:'demeanour', us:'demeanor'}
-words['disheveled'] = {ca:'dishevelled', gb:'dishevelled', us:'disheveled'}
-words['dishonor'] = {ca:'dishonour', gb:'dishonour', us:'dishonor'}
-words['dishonored'] = {ca:'dishonoured', gb:'dishonoured', us:'dishonored'}
-words['dispatch'] = {ca:'dispatch', gb:'dispatch, despatch', us:'dispatch'}
-words['emphasize'] = {ca:'emphasize', gb:'emphasise', us:'emphasize'}
-words['endeavor'] = {ca:'endeavour', gb:'endeavour', us:'endeavor'}
-words['endeavors'] = {ca:'endeavours', gb:'endeavours', us:'endeavors'}
-words['enroll'] = {ca:'enrol', gb:'enrol', us:'enroll'}
-words['favor'] = {ca:'favour', gb:'favour', us:'favor'}
-words['favorite'] = {ca:'favourite', gb:'favourite', us:'favorite'}
-words['favors'] = {ca:'favours', gb:'favours', us:'favors'}
-words['favored'] = {ca:'favoured', gb:'favoured', us:'favored'}
-words['fervor'] = {ca:'fervour', gb:'fervour', us:'fervor'}
-words['flavor'] = {ca:'flavour', gb:'flavour', us:'flavor'}
-words['fulfill'] = {ca:'fulfill', gb:'fulfil', us:'fulfill'}
-words['fulfillment'] = {ca:'fulfillment', gb:'fulfilment', us:'fulfillment'}
-words['gray'] = {ca:'grey', gb:'grey', us:'gray'}
-words['harbor'] = {ca:'harbour', gb:'harbour', us:'harbor'}
-words['harbors'] = {ca:'harbours', gb:'harbours', us:'harbors'}
-words['honor'] = {ca:'honour', gb:'honour', us:'honor'}
-words['honorable'] = {ca:'honourable', gb:'honourable', us:'honorable'}
-words['honorably'] = {ca:'honourably', gb:'honourably', us:'honorably'}
-words['honored'] = {ca:'honoured', gb:'honoured', us:'honored'}
-words['honoring'] = {ca:'honouring', gb:'honouring', us:'honoring'}
-words['honors'] = {ca:'honours', gb:'honours', us:'honors'}
-words['honors'] = {ca:'honours', gb:'honours', us:'honors'}
-words['jewelry'] = {ca:'jewellery', gb:'jewellery', us:'jewelry'}
-words['judgment'] = {ca:'judgment', gb:'judgement', us:'judgment'}
-words['judgments'] = {ca:'judgments', gb:'judgements', us:'judgments'}
-words['labor'] = {ca:'labour', gb:'labour', us:'labor'}
-words['labored'] = {ca:'laboured', gb:'laboured', us:'labored'}
-words['laborer'] = {ca:'labourer', gb:'labourer', us:'laborer'}
-words['laborers'] = {ca:'labourers', gb:'labourers', us:'laborers'}
-words['labors'] = {ca:'labours', gb:'labours', us:'labors'}
-words['leash'] = {ca:'leash', gb:'lead', us:'leash'}
-words['leveled'] = {ca:'levelled', gb:'levelled', us:'leveled'}
-words['license'] = {ca:'licence', gb:'licence', us:'license'}
-words['lowest story'] = {ca:'lowest storey', gb:'lowest storey', us:'lowest story'}
-words['mail'] = {ca:'mail', gb:'post', us:'mail'}
-words['marveled'] = {ca:'marvelled', gb:'marvelled', us:'marveled'}
-words['marveling'] = {ca:'marvelling', gb:'marvelling', us:'marveling'}
-words['marvelous'] = {ca:'marvellous', gb:'marvellous', us:'marvelous'}
-words['mobilize'] = {ca:'mobilize', gb:'mobilise', us:'mobilize'}
-words['mustache'] = {ca:'moustache', gb:'moustache', us:'mustache'}
-words['nearsighted'] = {ca:'near-sighted', gb:'short-sighted', us:'nearsighted'}
-words['neighbor'] = {ca:'neighbour', gb:'neighbour', us:'neighbor'}
-words['odor'] = {ca:'odour', gb:'odour', us:'odor'}
-words['offense'] = {ca:'offence', gb:'offence', us:'offense'}
-words['paneled'] = {ca:'panelled', gb:'panelled', us:'paneled'}
-words['pavement'] = {ca:'pavement', gb:'roadway', us:'pavement'}
-words['pretense'] = {ca:'pretence', gb:'pretence', us:'pretense'}
-words['realize'] = {ca:'realize', gb:'realise', us:'realize'}
-words['realized'] = {ca:'realized', gb:'realised', us:'realized'}
-words['realizes'] = {ca:'realizes', gb:'realises', us:'realizes'}
-words['recognize'] = {ca:'recognize', gb:'recognise', us:'recognize'}
-words['recognized'] = {ca:'recognized', gb:'recognise', us:'recognized'}
-words['recognizes'] = {ca:'recognizes', gb:'recognises', us:'recognizes'}
-words['reflection'] = {ca:'reflection', gb:'reflection', us:'reflection'}
-words['rent'] = {ca:'rent', gb:'let', us:'rent'}
-words['reveling'] = {ca:'revelling', gb:'revelling', us:'reveling'}
-words['rumor'] = {ca:'rumour', gb:'rumour', us:'rumor'}
-words['rumors'] = {ca:'rumours', gb:'rumours', us:'rumors'}
-words['savior'] = {ca:'saviour', gb:'saviour', us:'savior'}
-words['savor'] = {ca:'savour', gb:'savour', us:'savor'}
-words['scepter'] = {ca:'sceptre', gb:'sceptre', us:'scepter'}
-words['scrutinize'] = {ca:'scrutinize', gb:'scrutinise', us:'scrutinize'}
-words['shriveled'] = {ca:'shrivelled', gb:'shrivelled', us:'shriveled'}
-words['signaled'] = {ca:'signalled', gb:'signalled', us:'signaled'}
-words['signaling'] = {ca:'signalling', gb:'signalling', us:'signaling'}
-words['skillful'] = {ca:'skilful', gb:'skilful', us:'skillful'}
-words['smolder'] = {ca:'smoulder', gb:'smoulder', us:'smolder'}
-words['somber'] = {ca:'sombre', gb:'sombre', us:'somber'}
-words['splendor'] = {ca:'splendour', gb:'splendour', us:'splendor'}
-words['sulfur'] = {ca:'sulphur', gb:'sulphur', us:'sulfur'}
-words['third story'] = {ca:'third storey', gb:'third storey', us:'third story'}
-words['totaled'] = {ca:'totalled', gb:'totalled', us:'totaled'}
-words['tranquility'] = {ca:'tranquillity', gb:'tranquillity', us:'tranquility'}
-words['traveled'] = {ca:'travelled', gb:'travelled', us:'traveled'}
-words['traveler'] = {ca:'traveller', gb:'traveller', us:'traveler'}
-words['traveling'] = {ca:'travelling', gb:'travelling', us:'traveling'}
-words['tumor'] = {ca:'tumour', gb:'tumour', us:'tumor'}
-words['valor'] = {ca:'valour', gb:'valour', us:'valor'}
-words['vapor'] = {ca:'vapour', gb:'vapour', us:'vapor'}
-words['vigor'] = {ca:'vigour', gb:'vigour', us:'vigor'}
-words['willful'] = {ca:'wilful', gb:'wilful', us:'willful'}
-words['willfully'] = {ca:'wilfully', gb:'wilfully', us:'willfully'}
+var words = {};
+words["armor"] = { ca: "armour", gb: "armour", us: "armor" };
+words["armory"] = { ca: "armoury", gb: "armoury", us: "armory" };
+words["baptize"] = { ca: "baptize", gb: "baptise", us: "baptize" };
+words["behavior"] = { ca: "behaviour", gb: "behaviour", us: "behavior" };
+words["canceled"] = { ca: "cancelled", gb: "cancelled", us: "canceled" };
+words["center"] = { ca: "centre", gb: "centre", us: "center" };
+words["clamor"] = { ca: "clamour", gb: "clamour", us: "clamor" };
+words["counseled"] = { ca: "counselled", gb: "counselled", us: "counseled" };
+words["counselor"] = { ca: "counsellor", gb: "counsellor", us: "counselor" };
+words["defense"] = { ca: "defence", gb: "defence", us: "defense" };
+words["demeanor"] = { ca: "demeanour", gb: "demeanour", us: "demeanor" };
+words["disheveled"] = {
+	ca: "dishevelled",
+	gb: "dishevelled",
+	us: "disheveled",
+};
+words["dishonor"] = { ca: "dishonour", gb: "dishonour", us: "dishonor" };
+words["dishonored"] = {
+	ca: "dishonoured",
+	gb: "dishonoured",
+	us: "dishonored",
+};
+words["dispatch"] = {
+	ca: "dispatch",
+	gb: "dispatch, despatch",
+	us: "dispatch",
+};
+words["emphasize"] = { ca: "emphasize", gb: "emphasise", us: "emphasize" };
+words["endeavor"] = { ca: "endeavour", gb: "endeavour", us: "endeavor" };
+words["endeavors"] = { ca: "endeavours", gb: "endeavours", us: "endeavors" };
+words["enroll"] = { ca: "enrol", gb: "enrol", us: "enroll" };
+words["favor"] = { ca: "favour", gb: "favour", us: "favor" };
+words["favorite"] = { ca: "favourite", gb: "favourite", us: "favorite" };
+words["favoritism"] = {
+	ca: "favouritism",
+	gb: "favouritism",
+	us: "favoritism",
+};
+words["favors"] = { ca: "favours", gb: "favours", us: "favors" };
+words["favored"] = { ca: "favoured", gb: "favoured", us: "favored" };
+words["fervor"] = { ca: "fervour", gb: "fervour", us: "fervor" };
+words["flavor"] = { ca: "flavour", gb: "flavour", us: "flavor" };
+words["fulfill"] = { ca: "fulfill", gb: "fulfil", us: "fulfill" };
+words["fulfillment"] = {
+	ca: "fulfillment",
+	gb: "fulfilment",
+	us: "fulfillment",
+};
+words["gray"] = { ca: "grey", gb: "grey", us: "gray" };
+words["harbor"] = { ca: "harbour", gb: "harbour", us: "harbor" };
+words["harbors"] = { ca: "harbours", gb: "harbours", us: "harbors" };
+words["honor"] = { ca: "honour", gb: "honour", us: "honor" };
+words["honorable"] = { ca: "honourable", gb: "honourable", us: "honorable" };
+words["honorably"] = { ca: "honourably", gb: "honourably", us: "honorably" };
+words["honored"] = { ca: "honoured", gb: "honoured", us: "honored" };
+words["honoring"] = { ca: "honouring", gb: "honouring", us: "honoring" };
+words["honors"] = { ca: "honours", gb: "honours", us: "honors" };
+words["honors"] = { ca: "honours", gb: "honours", us: "honors" };
+words["jewelry"] = { ca: "jewellery", gb: "jewellery", us: "jewelry" };
+words["judgment"] = { ca: "judgment", gb: "judgement", us: "judgment" };
+words["judgments"] = { ca: "judgments", gb: "judgements", us: "judgments" };
+words["labor"] = { ca: "labour", gb: "labour", us: "labor" };
+words["labored"] = { ca: "laboured", gb: "laboured", us: "labored" };
+words["laborer"] = { ca: "labourer", gb: "labourer", us: "laborer" };
+words["laborers"] = { ca: "labourers", gb: "labourers", us: "laborers" };
+words["labors"] = { ca: "labours", gb: "labours", us: "labors" };
+words["leash"] = { ca: "leash", gb: "lead", us: "leash" };
+words["leveled"] = { ca: "levelled", gb: "levelled", us: "leveled" };
+words["license"] = { ca: "licence", gb: "licence", us: "license" };
+words["lowest story"] = {
+	ca: "lowest storey",
+	gb: "lowest storey",
+	us: "lowest story",
+};
+words["mail"] = { ca: "mail", gb: "post", us: "mail" };
+words["marveled"] = { ca: "marvelled", gb: "marvelled", us: "marveled" };
+words["marveling"] = { ca: "marvelling", gb: "marvelling", us: "marveling" };
+words["marvelous"] = { ca: "marvellous", gb: "marvellous", us: "marvelous" };
+words["mobilize"] = { ca: "mobilize", gb: "mobilise", us: "mobilize" };
+words["mustache"] = { ca: "moustache", gb: "moustache", us: "mustache" };
+words["nearsighted"] = {
+	ca: "near-sighted",
+	gb: "short-sighted",
+	us: "nearsighted",
+};
+words["neighbor"] = { ca: "neighbour", gb: "neighbour", us: "neighbor" };
+words["odor"] = { ca: "odour", gb: "odour", us: "odor" };
+words["offense"] = { ca: "offence", gb: "offence", us: "offense" };
+words["paneled"] = { ca: "panelled", gb: "panelled", us: "paneled" };
+words["pavement"] = { ca: "pavement", gb: "roadway", us: "pavement" };
+words["pretense"] = { ca: "pretence", gb: "pretence", us: "pretense" };
+words["realize"] = { ca: "realize", gb: "realise", us: "realize" };
+words["realized"] = { ca: "realized", gb: "realised", us: "realized" };
+words["realizes"] = { ca: "realizes", gb: "realises", us: "realizes" };
+words["recognize"] = { ca: "recognize", gb: "recognise", us: "recognize" };
+words["recognized"] = { ca: "recognized", gb: "recognise", us: "recognized" };
+words["recognizes"] = { ca: "recognizes", gb: "recognises", us: "recognizes" };
+words["reflection"] = { ca: "reflection", gb: "reflection", us: "reflection" };
+words["rent"] = { ca: "rent", gb: "let", us: "rent" };
+words["reveling"] = { ca: "revelling", gb: "revelling", us: "reveling" };
+words["rumor"] = { ca: "rumour", gb: "rumour", us: "rumor" };
+words["rumors"] = { ca: "rumours", gb: "rumours", us: "rumors" };
+words["savior"] = { ca: "saviour", gb: "saviour", us: "savior" };
+words["savor"] = { ca: "savour", gb: "savour", us: "savor" };
+words["scepter"] = { ca: "sceptre", gb: "sceptre", us: "scepter" };
+words["scrutinize"] = { ca: "scrutinize", gb: "scrutinise", us: "scrutinize" };
+words["shriveled"] = { ca: "shrivelled", gb: "shrivelled", us: "shriveled" };
+words["signaled"] = { ca: "signalled", gb: "signalled", us: "signaled" };
+words["signaling"] = { ca: "signalling", gb: "signalling", us: "signaling" };
+words["skillful"] = { ca: "skilful", gb: "skilful", us: "skillful" };
+words["smolder"] = { ca: "smoulder", gb: "smoulder", us: "smolder" };
+words["somber"] = { ca: "sombre", gb: "sombre", us: "somber" };
+words["splendor"] = { ca: "splendour", gb: "splendour", us: "splendor" };
+words["sulfur"] = { ca: "sulphur", gb: "sulphur", us: "sulfur" };
+words["third story"] = {
+	ca: "third storey",
+	gb: "third storey",
+	us: "third story",
+};
+words["totaled"] = { ca: "totalled", gb: "totalled", us: "totaled" };
+words["tranquility"] = {
+	ca: "tranquillity",
+	gb: "tranquillity",
+	us: "tranquility",
+};
+words["traveled"] = { ca: "travelled", gb: "travelled", us: "traveled" };
+words["traveler"] = { ca: "traveller", gb: "traveller", us: "traveler" };
+words["traveling"] = { ca: "travelling", gb: "travelling", us: "traveling" };
+words["tumor"] = { ca: "tumour", gb: "tumour", us: "tumor" };
+words["valor"] = { ca: "valour", gb: "valour", us: "valor" };
+words["vapor"] = { ca: "vapour", gb: "vapour", us: "vapor" };
+words["vigor"] = { ca: "vigour", gb: "vigour", us: "vigor" };
+words["willful"] = { ca: "wilful", gb: "wilful", us: "willful" };
+words["willfully"] = { ca: "wilfully", gb: "wilfully", us: "willfully" };
 
-
-export { words }
+export { words };
